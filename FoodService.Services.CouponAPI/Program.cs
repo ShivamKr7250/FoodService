94b129210c3a22361fc026b8d4648a6de4048837
Shivam <shivamkr930496@gmail.com>
2024-08-26 00:22:30 +0530

    All things are working fine

--- FoodService.Services.CouponAPI/Program.cs
+++ FoodService.Services.CouponAPI/Program.cs
@@ -4,6 +4,7 @@ using FoodService.Services.CouponAPI.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,11 +21,36 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[] { }
+        }
+    });
+});
 
-var secret = builder.Configuration.GetValue<string>("ApiSetting:Secret");
-var issuer = builder.Configuration.GetValue<string>("ApiSetting:Issuer");
-var audience = builder.Configuration.GetValue<string>("ApiSetting:Audience");
+var settingSection = builder.Configuration.GetSection("ApiSetting");
+
+var secret = settingSection.GetValue<string>("Secret");
+var issuer = settingSection.GetValue<string>("Issuer");
+var audience = settingSection.GetValue<string>("Audience");
 
 var key = Encoding.ASCII.GetBytes(secret);
 
--- FoodService.Web/Service/BaseService.cs
+++ FoodService.Web/Service/BaseService.cs
@@ -11,13 +11,14 @@ namespace FoodService.Web.Service
     public class BaseService : IBaseService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
-        public BaseService(IHttpClientFactory httpClientFactory)
+        private readonly ITokenProvider _tokenProvider;
+        public BaseService(IHttpClientFactory httpClientFactory, ITokenProvider tokenProvider)
         {
             _httpClientFactory = httpClientFactory;
+            _tokenProvider = tokenProvider;
         }
 
-        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true)
         {
             try
             {
@@ -25,6 +26,11 @@ namespace FoodService.Web.Service
                 HttpRequestMessage message = new();
                 message.Headers.Add("Accept", "application/json");
                 //token
+                if (withBearer)
+                {
+                    var token = _tokenProvider.GetToken();
+                    message.Headers.Add("Authorization", $"Bearer {token}");
+                }
                 message.RequestUri = new Uri(requestDto.Url);
                 if (requestDto.Data != null)
                 {
