696c46f12b89e45a65314fb8a21ba9c4b17f9924
Shivam <shivamkr930496@gmail.com>
2024-08-26 23:40:21 +0530

    removed cluter from couponapi program class

---
+++ FoodService.Services.CouponAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FoodService.Services.CouponAPI.Extensions
+{
+    public  static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthetication(this WebApplicationBuilder builder)
+        {
+            var settingSection = builder.Configuration.GetSection("ApiSetting");
+
+            var secret = settingSection.GetValue<string>("Secret");
+            var issuer = settingSection.GetValue<string>("Issuer");
+            var audience = settingSection.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    ValidateAudience = true,
+                };
+            });
+
+            return builder;
+        }
+    }
+}
--- FoodService.Services.CouponAPI/Program.cs
+++ FoodService.Services.CouponAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using FoodService.Services.CouponAPI;
 using FoodService.Services.CouponAPI.Data;
+using FoodService.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -46,30 +47,7 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-var settingSection = builder.Configuration.GetSection("ApiSetting");
-
-var secret = settingSection.GetValue<string>("Secret");
-var issuer = settingSection.GetValue<string>("Issuer");
-var audience = settingSection.GetValue<string>("Audience");
-
-var key = Encoding.ASCII.GetBytes(secret);
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidIssuer = issuer,
-        ValidAudience = audience,
-        ValidateAudience = true,
-    };
-});
+builder.AddAppAuthetication();
 
 builder.Services.AddAuthorization();
 
