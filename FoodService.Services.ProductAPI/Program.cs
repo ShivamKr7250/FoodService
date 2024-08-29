9b1b004ac304b208c26f4c28e7edfc0657a036c9
Shivam <shivamkr930496@gmail.com>
2024-08-29 18:30:59 +0530

    streak

--- FoodService.Services.ProductAPI/Program.cs
+++ FoodService.Services.ProductAPI/Program.cs
@@ -1,6 +1,17 @@
+using AutoMapper;
+using FoodService.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
